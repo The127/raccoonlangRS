592723fd818467b523e8bf29b502c53ebf22a064
karo <karolin.kostial@gmail.com>
2024-12-18 12:47:11 +0100

    implement nested marks

--- src/mark_iterator.rs
+++ src/mark_iterator.rs
@@ -6,7 +6,7 @@ where
 {
     inner: I,
     buffers: Vec<VecDeque<I::Item>>,
-    read_buffer: bool,
+    current_read_buffer: Option<VecDeque<I::Item>>,
 }
 
 impl<I> MarkIterator<I>
@@ -18,19 +18,17 @@ where
     }
 
     pub fn reset(&mut self) {
-        if self.read_buffer {
-            if let Some(mut top) = self.buffers.pop(){
-                if let Some(new_top) = self.buffers.last_mut() {
-                    new_top.append(&mut top);
-                }
-            }
+        if let Some(current) = &mut self.current_read_buffer {
+            let mut new = self.buffers.pop().expect("no mark to reset to");
+            new.append(current);
+            self.current_read_buffer = Some(new);
+        } else {
+            self.current_read_buffer = Some(self.buffers.pop().expect("no mark to reset to"));
         }
-
-        self.read_buffer = true;
     }
 
     pub fn discard(&mut self) {
-        self.buffers.pop();
+        self.buffers.pop().expect("no mark to discard");
     }
 }
 
@@ -40,7 +38,7 @@ where
 {
     MarkIterator {
         inner: inner,
-        read_buffer: false,
+        current_read_buffer: None,
         buffers: vec![],
     }
 }
@@ -52,16 +50,19 @@ where
     type Item = I::Item;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.read_buffer {
-            if let Some(item) = self.buffers.last_mut().expect("stack is empty").pop_front() {
-                let num_buffers = self.buffers.len();
-                if num_buffers > 1 {
-                    self.buffers.get_mut(num_buffers - 2).unwrap().push_back(item);
+        if let Some(buf) = &mut self.current_read_buffer {
+            if let Some(item) = buf.pop_front() {
+
+                if let Some(top) = self.buffers.last_mut() {
+                    top.push_back(item);
                 }
+
                 return Some(item);
             }
 
-            self.read_buffer = false;
+            if buf.is_empty() {
+                self.current_read_buffer = None;
+            }
         }
 
         if self.buffers.is_empty() {
@@ -69,7 +70,10 @@ where
         }
 
         if let Some(result) = self.inner.next() {
-            self.buffers.last_mut().expect("stack is empty").push_back(result);
+            self.buffers
+                .last_mut()
+                .expect("stack is empty")
+                .push_back(result);
             return Some(result);
         }
 
@@ -82,14 +86,30 @@ mod test {
     use super::*;
     use paste::paste;
 
-    macro_rules! mark_iter_tests {
-        ($($name:ident: $input:tt -> [$($step:ident),*] -> $expected:tt;)*) => {
-        $(
+
+    macro_rules! mark_iter_test_helper {
+        ($name:ident: [$($input:expr),*] -> [$($step:ident),*] -> panic) => {
+        paste! {
+            #[test]
+            #[should_panic]
+            fn [<test_ $name >] () {
+                // arrange
+                let source = vec![$($input,)*];
+                let mut iter = mark(source.clone().into_iter());
+
+                // act
+                $(
+                    iter.$step();
+                )*
+            }
+        }
+        };
+        ($name:ident: [$($input:expr),*] -> [$($step:ident),*] -> [$($expected:expr),*]) => {
         paste! {
             #[test]
             fn [<test_ $name >] () {
                 // arrange
-                let source = vec! $input;
+                let source = vec![$($input,)*];
                 let mut iter = mark(source.clone().into_iter());
 
                 // act
@@ -100,22 +120,40 @@ mod test {
                 let remaining: Vec<_> = iter.collect();
 
                 // assert
-                assert_eq!(remaining, vec! $expected);
+                assert_eq!(remaining, vec![$($expected,)*]);
             }
         }
-        )*
+        };
+    }
+    macro_rules! mark_iter_tests {
+        ($($name:ident: $input:tt -> $steps:tt -> $result:tt;)*) => {
+            $(
+                mark_iter_test_helper! { $name: $input -> $steps -> $result }
+            )*
         }
     }
 
-    mark_iter_tests!{
-        wraps_internal_iter: [1, 2, 3, 4, 5] -> [] -> [1, 2, 3, 4, 5];
+    mark_iter_tests! {
+        wraps_internal_iter: [1,2,3,4,5] -> [] -> [1,2,3,4,5];
         strings: ["foo", "bar", "foobar", "fuzz"] -> [] -> ["foo", "bar", "foobar", "fuzz"];
         mark_has_no_direct_effect: [1,2,3,4,5] -> [next, mark] -> [2,3,4,5];
-        mark_and_reset_returns_jumps_to_mark: [1,2,3,4,5] -> [next, mark, next, reset] -> [2, 3, 4, 5];
-        mark_and_reset_returns_jumps_to_start: [1,2,3,4,5] -> [mark, next, next, reset] -> [1, 2, 3, 4, 5];
-        mark_and_discard_continues: [1,2,3,4,5] -> [next, mark, next, discard] -> [3, 4, 5];
-        mark_and_discard_remarks: [1,2,3,4,5] -> [next, mark, next, discard, next, mark, next, reset] -> [4, 5];
+        mark_and_reset_returns_jumps_to_mark: [1,2,3,4,5] -> [next, mark, next, reset] -> [2,3,4,5];
+        mark_and_reset_returns_jumps_to_start: [1,2,3,4,5] -> [mark, next, next, reset] -> [1,2,3,4,5];
+        mark_and_discard_continues: [1,2,3,4,5] -> [next, mark, next, discard] -> [3,4,5];
+        mark_and_discard_remarks: [1,2,3,4,5] -> [next, mark, next, discard, next, mark, next, reset] -> [4,5];
+        mark_and_discard_and_next: [1,2,3,4,5] -> [next, mark, next, discard, next] -> [4,5];
         mark_and_reset_nested: [1,2,3,4,5] -> [next, mark, next, mark, next, next, reset, next, reset] -> [2,3,4,5];
         mark_and_reset_nested_unbalanced: [1,2,3,4,5] -> [next, mark, next, mark, next, next, reset] -> [3,4,5];
+        mark_and_reset_nested_middle: [1,2,3,4,5] -> [next, mark, next, mark, next, mark, next, reset, next, reset] -> [3,4,5];
+        mark_and_discard_nested: [1,2,3,4,5] -> [next, mark, next, mark, next, next, discard, next, discard] -> [];
+        mark_and_discard_nested_unbalanced: [1,2,3,4,5] -> [next, mark, next, mark, next, next, discard] -> [5];
+        mark_after_reset: [1,2,3,4,5] -> [mark, next, mark, next, reset, next, mark, next, reset] -> [3,4,5];
+        reset_then_next_past: [1,2,3,4,5] -> [mark, next, next, reset, next, next, next] -> [4,5];
+        multiple_mark_and_reset: [1,2,3,4,5] -> [mark, next, mark, next, next, reset, next, next, reset, next] -> [2,3,4,5];
+        reset_without_mark: [1,2,3,4,5] -> [reset] -> panic;
+        discard_without_mark: [1,2,3,4,5] -> [discard] -> panic;
+        reset_too_often: [1,2,3,4,5] -> [mark, reset, reset] -> panic;
+        discard_too_often: [1,2,3,4,5] -> [mark, discard, discard] -> panic;
+        discard_after_reset_too_often: [1,2,3,4,5] -> [mark, reset, discard] -> panic;
     }
 }
