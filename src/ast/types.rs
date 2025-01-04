685ef4a4ca03b2dde8d378ff4e94b149c3eab2e4
karo <karolin.kostial@gmail.com>
2025-01-04 15:04:34 +0100

    add tests for rooted paths

--- src/ast/types.rs
+++ src/ast/types.rs
@@ -67,4 +67,83 @@ mod test {
             rooted: false,
         }));
     }
+
+    #[test]
+    fn transform_named_type_non_rooted_multiple_identifier() {
+        // arrange
+        let mut sources = SourceCollection::new();
+        let id_span1 = sources.load_content("foo".to_string());
+        let id_span2 = sources.load_content("bar".to_string());
+        let parser_type = TypeNode::Named(NamedTypeNode{
+            span: id_span1 + id_span2,
+            path: PathNode {
+                span: id_span1 + id_span2,
+                parts: test_tokens!(Identifier:id_span1, Identifier:id_span2),
+                is_rooted: false,
+            },
+        });
+
+        // act
+        let result = transform_type(&parser_type, &sources);
+
+        // assert
+        assert_eq!(result, Type::Named(NamedType{
+            span: id_span1 + id_span2,
+            path: vec![ustr("foo"), ustr("bar")],
+            rooted: false,
+        }));
+    }
+
+    #[test]
+    fn transform_named_type_rooted_single_identifier() {
+        // arrange
+        let mut sources = SourceCollection::new();
+        let root_span = sources.load_content("::".to_string());
+        let id_span = sources.load_content("foo".to_string());
+        let parser_type = TypeNode::Named(NamedTypeNode{
+            span: root_span + id_span,
+            path: PathNode {
+                span: root_span + id_span,
+                parts: test_tokens!(Identifier:id_span),
+                is_rooted: true,
+            },
+        });
+
+        // act
+        let result = transform_type(&parser_type, &sources);
+
+        // assert
+        assert_eq!(result, Type::Named(NamedType{
+            span: root_span + id_span,
+            path: vec![ustr("foo")],
+            rooted: true,
+        }));
+    }
+
+    #[test]
+    fn transform_named_type_rooted_multiple_identifier() {
+        // arrange
+        let mut sources = SourceCollection::new();
+        let root_span = sources.load_content("::".to_string());
+        let id_span1 = sources.load_content("foo".to_string());
+        let id_span2 = sources.load_content("bar".to_string());
+        let parser_type = TypeNode::Named(NamedTypeNode{
+            span: root_span + id_span1 + id_span2,
+            path: PathNode {
+                span: root_span + id_span1 + id_span2,
+                parts: test_tokens!(Identifier:id_span1, Identifier:id_span2),
+                is_rooted: true,
+            },
+        });
+
+        // act
+        let result = transform_type(&parser_type, &sources);
+
+        // assert
+        assert_eq!(result, Type::Named(NamedType{
+            span: root_span + id_span1 + id_span2,
+            path: vec![ustr("foo"), ustr("bar")],
+            rooted: true,
+        }));
+    }
 }
