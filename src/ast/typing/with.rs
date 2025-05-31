3cbe5da7de65661dda7d7fcdff387a9c1a569ec3
karo <karolin.kostial@gmail.com>
2025-05-31 15:24:44 +0200

    start on basic first test for with typecheck

--- src/ast/typing/with.rs
+++ src/ast/typing/with.rs
@@ -8,5 +8,50 @@ pub(super) fn typecheck_with(
     scope: &TypeScope,
     errors: &mut Errors,
 ) -> TypeRef {
-    todo!();
+    panic!("")
+}
+
+#[cfg(test)]
+mod test {
+    use ustr::ustr;
+    use crate::ast::expressions::arg::Arg;
+    use crate::ast::expressions::Expression;
+    use crate::ast::path::Path;
+    use crate::ast::typing::typecheck_expression;
+    use crate::errors::Errors;
+    use crate::parser::ToSpanned;
+    use crate::scope::type_::TypeScope;
+    use crate::types::type_ref::TypeRef;
+
+    #[test]
+    fn target_without_values() {
+        // arrange
+        let mut expr = Expression::with(
+            0,
+            Expression::access(1, Path::name("a")),
+            vec![
+                Arg::shorthand(
+                    2,
+                    ustr("b").spanned(2),
+                    Expression::access(2, Path::name("b"))
+                ),
+                Arg::named(
+                    3,
+                    ustr("c").spanned(3),
+                    Expression::i32_literal(4, 1)
+                ),
+            ]);
+
+        let mut errors = Errors::new();
+        let scope = TypeScope::new();
+
+        // act
+        typecheck_expression(&mut expr, &scope, &mut errors);
+
+        // assert
+        assert_eq!(
+            expr.type_ref(),
+            Some(TypeRef::i32()),
+        );
+    }
 }
